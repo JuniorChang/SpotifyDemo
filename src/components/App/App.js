01c79e9ccc2f145b251cd7a27d5824d55037729f
Junior <changjiale@gmail.com>
2022-03-21 20:34:25 +0800

    error displaying

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,19 +7,19 @@ import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
   render() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-         {/* <SearchResults /> */}
-          {/* <Playlist /> */}
-         </div>
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+          {/* <SearchResults /> */}
+            {/* <Playlist /> */}
+          </div>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default App;
