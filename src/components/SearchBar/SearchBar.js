74abeb865a0f223906f7c2b3d102cc9e97cde33a
Junior <changjiale@gmail.com>
2022-03-21 23:29:43 +0800

    render issue solved

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-  return () { 
+  render () { 
     return (
     <div className="SearchBar">
   <input placeholder="Enter A Song, Album, or Artist" />
   <button className="SearchButton">SEARCH</button>
+  
 </div>
     )}
 }
---
+++ src/components/Test/Test.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Test = () => {
+    console.log("testing render")
+    return (
+        <div className="test">
+            <p> testing correct</p>
+        </div>
+    )
+}
